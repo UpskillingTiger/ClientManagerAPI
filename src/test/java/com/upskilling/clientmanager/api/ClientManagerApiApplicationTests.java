cbbe3170a6813e1d904a5ec1814dc7f717825462
Kotha, Vara <Varaprasad.Kotha@McKesson.com>
2020-08-04 22:38:58 -0700

    initial commit

---
+++ src/test/java/com/upskilling/clientmanager/api/ClientManagerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.upskilling.clientmanager.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientManagerApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
